976ee5bdf50fe5832fbf582c304c13627eecdee5
Shibin8156 <134442322+Shibin8156@users.noreply.github.com>
2023-05-24 00:55:21 -0400

    Add files via upload

---
+++ MyPrograms/PYTHON/12thdaypython.py
@@ -0,0 +1,71 @@
+# Q1.
+# create a class called polygon defined as follows
+# a)data attributes to store the no.of sides,(take input frome user)
+# b)create another class called triangle which inherit from pollygon to find the area of triangle
+
+# class pollygon:
+#     def __init__(self,no of sides):
+#           self.n=no of sides
+#           self.side=[0 for i in range(no_of_sides)]
+
+
+
+# t=0
+# while t<=10:
+#     print(t)
+#     t+=1 
+      
+# t=0
+# while t<10:
+#     t+=1 
+#     print(t)
+
+# t=1
+# while t<10:
+#     print(t)
+#     t+=3
+
+# t=0
+# while t<=20:
+#     print(t)
+#     t+=5 
+
+#  Q1.write a programe to display the seven multiples of 7???
+
+# y=0
+# while y<=49:
+#     print(y)
+#     y+=7
+
+# r=1
+# while r<=7:
+#     print(r*7)
+#     r+=1
+
+# t=1
+# while t<=8:
+#     print(t*8)
+#     t+=1
+
+#Q2. programm that append the square ofeach  number toa new list
+# x= [2,7,8,4,3,1]      ([4,49,64,16,9])
+
+
+# x=[2,7,8,4,3,1]
+# y=[]
+# for sbn in x:
+#     d=sbn*sbn
+#     y.append(d)
+# print(y)
+
+
+# x=[2,7,8,4,3,1]
+# y=[]
+# for sbn in x:
+#     y.append(sbn**2)
+# print(x)
+# print(y)
+
+#PYTHON EXERCISE
+
+# Q1. CALCULATE THE SUM OF ALL NUMBERS FROM 1 TO A GIVEN NUMBER.
